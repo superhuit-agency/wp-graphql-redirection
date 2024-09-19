a6b42619cc052cea30c0611e4c101ea02b279f9f
Felipe Paul Martins <felipe@superhuit.ch>
2024-09-19 10:47:28 -0400

    fix: :bug: isRedirected with false positive
    If any regex redirection was defined, the isRedirected field returned false positive fall uris
    Thanks to @garygrand for the discover

--- include/Type/InterfaceType/NodeWithIsRedirect.php
+++ include/Type/InterfaceType/NodeWithIsRedirect.php
@@ -21,8 +21,24 @@ class NodeWithIsRedirected {
 						'type'        => 'Boolean',
 						'description' => __( 'Is the current node URI redirected somewhere.', 'wpgraphql-redirection' ),
 						'resolve'     => function ( $source, $args ) {
-							$items = Red_Item::get_for_matched_url( $source->link );
-							return count($items) > 0;
+							/**
+							 * Replicates what Redirection does to check if the current URL is redirected
+							 * @see https://github.com/johngodley/redirection/blob/8a4b91fa8eabdf0c0d9d03cbbddcbfb0d91e046d/modules/wordpress.php#L334-L340
+							 */
+							$isRedirected = false;
+							$original_url = $source->link;
+							$decoded_url  = rawurldecode( $original_url );
+
+							$items = Red_Item::get_for_matched_url( $decoded_url );
+
+							$i = 0;
+							while ( $isRedirected === false && $i < count($items) ) {
+								$action       = $items[$i]->get_match( $decoded_url, $original_url );
+								$isRedirected = $action !== false;
+								$i++;
+							}
+
+							return $isRedirected;
 						},
 					],
 				],
